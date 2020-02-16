c8106cc68c43513bedccbf04bc35300451163b14
Frank Doyle <frankdoylezw@gmail.com>
2020-02-16 22:09:39 +0000

    Adding a new script and the word document template required

---
+++ newstory_a.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+if [ $# == 0 ]; then
+    echo "Usage: $0 param1"
+    echo "* param1: <This is the story number of the user story you are testing>"
+    exit 1
+fi
+
+
+
+story_number="$1"
+mkdir -p ${story_number}/{Documentation,Test_Evidence} && cd "$_"
+
+cwd=$(pwd)
+clear
+echo
+echo
+echo
+echo "Current working directory is '$cwd'"
+
+cp /c/Users/frank/bin/Userform\ Template.dotm $cwd 
+
+
+echo "Folders for story '$story_number' created!"
+
